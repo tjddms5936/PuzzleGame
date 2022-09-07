7eb3162a3f8e65219e389fb518c1350d9508717a
ParkSungEun <tjddms5936@naver.com>
2022-09-07 22:36:40 +0900

    호스팅 메커니즘 구현

--- Source/PuzzleGame/PuzzlePlatformGameInstance.cpp
+++ Source/PuzzleGame/PuzzlePlatformGameInstance.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PuzzlePlatformGameInstance.h"
+#include "Engine/World.h"
 
 UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer) 
 {
@@ -11,5 +12,20 @@ UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitialize
 void UPuzzlePlatformGameInstance::Init()
 {
 	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+}
+
+void UPuzzlePlatformGameInstance::HostServer()
+{
+	// GetEngine() : GameInstance만의 메서드. 
+	GEngine->AddOnScreenDebugMessage(1, 2, FColor::Green, TEXT("Hosting"));
+	
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+}
+
+void UPuzzlePlatformGameInstance::Join(const FString& Address)
+{
+	GEngine->AddOnScreenDebugMessage(1, 2, FColor::Black, FString::Printf(TEXT("Joining %s"), *Address));
 }
 
--- Source/PuzzleGame/PuzzlePlatformGameInstance.h
+++ Source/PuzzleGame/PuzzlePlatformGameInstance.h
@@ -16,5 +16,11 @@ class PUZZLEGAME_API UPuzzlePlatformGameInstance : public UGameInstance
 
 	UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer);
 
-	virtual void Init();
+	virtual void Init(); // BeginPlay 느낌
+
+	UFUNCTION(Exec) // Exec : 이 기능은 게임 내 콘솔에서 실행할 수 있습니다. Exec 명령은 특정 클래스 내에서 선언된 경우에만 작동합니다.
+	void HostServer();
+
+	UFUNCTION(Exec)
+	void Join(const FString& Address);
 };
